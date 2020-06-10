f5182a4e25cac60ed7d121ab84c836232872a1b3
Ke G <giki.biu@gmail.com>
2020-06-10 18:12:20 +0800

    add example code

--- Example-iOS/ViewController.m
+++ Example-iOS/ViewController.m
@@ -7,17 +7,82 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "Masonry.h"
+#import "GDorisPhotoPickerController.h"
+#import "GDorisSwipeGestureTransition.h"
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic, strong) UITableView * tableView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self.view addSubview:({
+        UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        tableView.delegate = self;
+        tableView.dataSource = self;
+        tableView.estimatedRowHeight = 0;
+        tableView.estimatedSectionFooterHeight = 0;
+        tableView.estimatedSectionHeaderHeight = 0;
+        _tableView = tableView;
+        tableView;
+    })];
+    [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
 }
 
 
+#pragma mark -- TableView DataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"tableViewCell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"tableViewCell"];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row];
+    cell.textLabel.textColor = UIColor.blackColor;
+    return cell;
+}
+
+#pragma mark -- TableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self openPhotoPicker];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 70;
+}
+
+- (void)openPhotoPicker
+{
+    /*
+    GDorisPhotoPickerController * picker = [[GDorisPhotoPickerController alloc] init];
+    UINavigationController *  nav = [[UINavigationController alloc] initWithRootViewController:picker];
+    nav.modalPresentationStyle = UIModalPresentationCustom;
+    GDorisSwipeGestureTransition * swipeTransition = [GDorisSwipeGestureTransition swipeGestureTransitionWithTarget:picker];
+    swipeTransition.needsTopMask = YES;
+    swipeTransition.containerOffset = 44;
+    nav.transitioningDelegate = swipeTransition;
+    nav.modalPresentationStyle = UIModalPresentationCustom;
+    nav.modalPresentationCapturesStatusBarAppearance = YES;
+    [self presentViewController:nav animated:YES completion:nil];
+     */
+}
 @end
--- Podfile
+++ Podfile
@@ -1,19 +1,25 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
+def mainApp_pods
+   pod 'YYText'
+   pod 'YYImage'
+   pod 'YYImage/WebP'
+   pod 'SDWebImage', '~> 5.0'
+   pod 'SDWebImageWebPCoder', :git => 'https://github.com/SDWebImage/SDWebImageWebPCoder.git', :branch => 'master'
+   pod 'SDWebImageYYPlugin'
+   pod 'Masonry'
+   pod 'FLAnimatedImage'
+
+end
+
 target 'GDorisPhotoKit' do
-
-  pod 'YYText'
-  pod 'YYImage'
-  pod 'YYImage/WebP'
-  pod 'SDWebImage', '~> 5.0'
-  pod 'SDWebImageWebPCoder', :git => 'https://github.com/SDWebImage/SDWebImageWebPCoder.git', :branch => 'master'
-  pod 'SDWebImageYYPlugin'
-  pod 'Masonry'
-  pod 'FLAnimatedImage'
-
+  mainApp_pods
+ 
   target 'GDorisPhotoKitTests' do
     # Pods for testing
   end
-
+  target 'Example-iOS' do
+    mainApp_pods
+  end
 end
